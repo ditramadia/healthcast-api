f20c3de580facef1cb5ded02ef45e43f31f5ea3f
ditramadia <ditrarizqaamadia@gmail.com>
2024-11-24 19:19:37 +0700

    feat: add edit user profile endpoint

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { isUserUidExists, getUserDataByUid } = require("./user.service");
+const {
+  isUserUidExists,
+  getUserData,
+  updateUserData,
+} = require("./user.service");
 const router = express.Router();
 
 /**
- * GET USER BY UID
+ * GET USER PROFILE
  * GET /users/:uid
  */
 router.get("/:uid", async (req, res) => {
@@ -16,7 +20,7 @@ router.get("/:uid", async (req, res) => {
       });
     }
 
-    const userData = await getUserDataByUid(uid);
+    const userData = await getUserData(uid);
 
     res.status(200).json({
       message: `User with uid ${uid} retrieved successfully`,
@@ -25,6 +29,47 @@ router.get("/:uid", async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: `Error retrieving user with uid ${uid}`,
+      error: error.message,
+    });
+  }
+});
+
+/**
+ * EDIT USER PROFILE
+ * POST /users/:uid
+ */
+router.post("/:uid", async (req, res) => {
+  const { uid } = req.params;
+  const { fullName, gender, age } = req.body;
+
+  try {
+    const isUserExists = await isUserUidExists(uid);
+    if (!isUserExists) {
+      return res.status(404).json({
+        message: `User with uid ${uid} does not exists`,
+      });
+    }
+
+    if (!fullName) {
+      return res.status(400).json({
+        message: "Missing required field: Full Name",
+      });
+    }
+
+    const updatedProfile = {
+      fullName,
+    };
+    if (gender) updatedProfile.gender = gender;
+    if (age) updatedProfile.age = age;
+
+    await updateUserData(uid, updatedProfile);
+
+    res.status(200).json({
+      message: `User with uid ${uid} updated successfully`,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `Error updating user with uid ${uid}`,
       error: error.message,
     });
   }
