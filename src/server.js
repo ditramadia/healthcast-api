528507afa2737e4cd7256887b22dc5e29a9ce2a5
ditramadia <ditrarizqaamadia@gmail.com>
2024-11-27 14:35:53 +0700

    feat: add dotenv for env

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
-// const authController = require("./auth/auth.controller");
-// const userController = require("./user/user.controller");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const authController = require("./auth/auth.controller");
+const userController = require("./user/user.controller");
 
 const app = express();
 app.use(express.json());
@@ -11,8 +14,8 @@ app.get("/", async (req, res) => {
   res.send("This is the API for HealthCast App");
 });
 
-// app.use("", authController);
-// app.use("/users", userController);
+app.use("", authController);
+app.use("/users", userController);
 
 app.listen(PORT, () => {
   console.log(`Express API running in port ${PORT}`);
