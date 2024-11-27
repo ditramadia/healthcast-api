0634ac0ae4dd502f9b6ac36459a60705930daa6e
ditramadia <ditrarizqaamadia@gmail.com>
2024-11-27 16:22:05 +0700

    fix: refactor serviceAccount

--- src/config/firebase-admin.js
+++ src/config/firebase-admin.js
@@ -1,6 +1,12 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
+// const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
+
+const serviceAccount = {
+  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  clientEmail: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+  privateKey: process.env.GOOGLE_CLOUD_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
