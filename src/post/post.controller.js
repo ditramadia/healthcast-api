dd1500f36f71365cd67ace05926e405d5e96afd3
ditramadia <ditrarizqaamadia@gmail.com>
2024-12-02 21:50:03 +0700

    feat: update get all posts endpoint

--- src/post/post.controller.js
+++ src/post/post.controller.js
@@ -1,135 +1,126 @@
-const express = require('express');
-const { 
-    getPosts,
-    getPost,
-    createPost,
-    createComment,
-    updatePost,
-    deletePost,
-    getComments,
-    isPostIdExists
-    
-} = require('./post.service');
-const {isUserUidExists} = require('../user/user.service');
+const express = require("express");
+const { getPosts } = require("./post.service");
 const router = express.Router();
-/*
-Get all posts
-GET /posts
-*/
-router.get('/', async (req, res) => {
-    const { page = 1, limit = 5} = req.query;
-    try {
-        const posts = await getPosts(page, limit);
-        res.status(200).json({
-            message: "Posts retrieved successfully",
-            data: posts
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Error retrieving posts",
-            error: error.message
-        });
-    }
+
+/**
+ * GET ALL POSTS
+ * GET /posts
+ */
+router.get("/", async (req, res) => {
+  const { page = 1, limit = 5 } = req.query;
+
+  try {
+    const posts = await getPosts(page, limit);
+    res.status(200).json({
+      message: "Posts retrieved successfully",
+      data: posts,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error retrieving posts",
+      error: error.message,
+    });
+  }
 });
 /*
 
 Create new post
 POST /posts
 */
-router.post('/', async (req, res) => {
-    const { user, title, description, image_url } = req.body;
-    try {
-        const isUserExists = await isUserUidExists(user);
-        if (!isUserExists) {
-            return res.status(404).json({
-                message: `User with uid ${user} does not exists`
-            });
-        }
-        await createPost({ user, title, description, image_url});
-        res.status(201).json({
-            message: "Post created successfully",
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Error creating post",
-            error: error.message
-        });
-    }
-});
+// router.post("/", async (req, res) => {
+//   const { user, title, description, image_url } = req.body;
+//   try {
+//     const isUserExists = await isUserUidExists(user);
+//     if (!isUserExists) {
+//       return res.status(404).json({
+//         message: `User with uid ${user} does not exists`,
+//       });
+//     }
+//     await createPost({ user, title, description, image_url });
+//     res.status(201).json({
+//       message: "Post created successfully",
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Error creating post",
+//       error: error.message,
+//     });
+//   }
+// });
 /*
 Get post by id
 GET /posts/:id
 */
-router.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const isPostExists = await isPostIdExists(id);
-        if (!isPostExists) {
-            return res.status(404).json({
-                message: `Post with id ${id} does not exists`
-            });
-        }
-        const post = await getPost(id);
-        res.status(200).json({
-            message: "Post retrieved successfully",
-            data: post
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Error retrieving post",
-            error: error.message
-        });
-    }
-});
+// router.get("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const isPostExists = await isPostIdExists(id);
+//     if (!isPostExists) {
+//       return res.status(404).json({
+//         message: `Post with id ${id} does not exists`,
+//       });
+//     }
+//     const post = await getPost(id);
+//     res.status(200).json({
+//       message: "Post retrieved successfully",
+//       data: post,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Error retrieving post",
+//       error: error.message,
+//     });
+//   }
+// });
 /*
 Edit Post by id
 PUT /posts/:id
 */
-router.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    const { title, description, image_url} = req.body;
-    try {
-        const isPostExists = await isPostIdExists(id);
-        if (!isPostExists) {
-            return res.status(404).json({
-                message: `Post with id ${id} does not exists`
-            });
-        }
-        await updatePost(id, {title, description, image_url});
-        res.status(200).json({
-            message: "Post updated successfully"
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Error updating post",
-            error: error.message
-        });
-    }
-});
+// router.put("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   const { title, description, image_url } = req.body;
+//   try {
+//     const isPostExists = await isPostIdExists(id);
+//     if (!isPostExists) {
+//       return res.status(404).json({
+//         message: `Post with id ${id} does not exists`,
+//       });
+//     }
+//     await updatePost(id, { title, description, image_url });
+//     res.status(200).json({
+//       message: "Post updated successfully",
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Error updating post",
+//       error: error.message,
+//     });
+//   }
+// });
 /*
 Delete Post by id
 DELETE /posts/:id
 */
-router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const isPostExists = await isPostIdExists(id);
-        if (!isPostExists) {
-            return res.status(404).json({
-                message: `Post with id ${id} does not exists`
-            });
-        }
-        await deletePost(id);
-        res.status(200).json({
-            message: "Post deleted successfully"
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Error deleting post",
-            error: error.message
-        });
-    }
-});
+// router.delete("/:id", async (req, res) => {
+//   const { id } = req.params;
+//   try {
+//     const isPostExists = await isPostIdExists(id);
+//     if (!isPostExists) {
+//       return res.status(404).json({
+//         message: `Post with id ${id} does not exists`,
+//       });
+//     }
+//     await deletePost(id);
+//     res.status(200).json({
+//       message: "Post deleted successfully",
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Error deleting post",
+//       error: error.message,
+//     });
+//   }
+// });
 /*
 Like post
 PUT /posts/:id/likes
@@ -140,50 +131,50 @@ PUT /posts/:id/dislikes
 Get all comments
 GET /posts/:id/comments
 */
-router.get('/:id/comments', async (req, res) => {
-    const { id } = req.params;
-    const { page = 1, limit = 5} = req.query;
-    try {
-        const isPostExists = await isPostIdExists(id);
-        if (!isPostExists) {
-            return res.status(404).json({
-                message: `Post with id ${id} does not exists`
-            });
-        }
-        const comments = await getComments(id, page, limit);
-        res.status(200).json({
-            message: "Comments retrieved successfully",
-            data: comments
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Error retrieving comments",
-            error: error.message
-        });
-    }
-});
+// router.get("/:id/comments", async (req, res) => {
+//   const { id } = req.params;
+//   const { page = 1, limit = 5 } = req.query;
+//   try {
+//     const isPostExists = await isPostIdExists(id);
+//     if (!isPostExists) {
+//       return res.status(404).json({
+//         message: `Post with id ${id} does not exists`,
+//       });
+//     }
+//     const comments = await getComments(id, page, limit);
+//     res.status(200).json({
+//       message: "Comments retrieved successfully",
+//       data: comments,
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Error retrieving comments",
+//       error: error.message,
+//     });
+//   }
+// });
 
-router.post('/:id/comments', async (req, res) => {
-    const { id } = req.params;
-    const { user, comment} = req.body;
-    try {
-        const isPostExists = await isPostIdExists(id);
-        if (!isPostExists) {
-            return res.status(404).json({
-                message: `Post with id ${id} does not exists`
-            });
-        }
-        await createComment(id, { user, comment });
-        res.status(201).json({
-            message: "Comment created successfully"
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: "Error creating comment",
-            error: error.message
-        });
-    }
-});
+// router.post("/:id/comments", async (req, res) => {
+//   const { id } = req.params;
+//   const { user, comment } = req.body;
+//   try {
+//     const isPostExists = await isPostIdExists(id);
+//     if (!isPostExists) {
+//       return res.status(404).json({
+//         message: `Post with id ${id} does not exists`,
+//       });
+//     }
+//     await createComment(id, { user, comment });
+//     res.status(201).json({
+//       message: "Comment created successfully",
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       message: "Error creating comment",
+//       error: error.message,
+//     });
+//   }
+// });
 
 module.exports = router;
 /*
--- src/post/post.service.js
+++ src/post/post.service.js
@@ -1,85 +1,115 @@
-const { post } = require("../auth/auth.controller");
-const {
-    getPostById,
-    getPostComments,
-    getAllPosts,
-    getCommentById,
-    createNewPost,
-    createNewComment,
-    updatePostById,
-    deletePostById,
-} = require("./post.repository");
-
-const isPostIdExists = async (postId) => {
-    const postSnapshot = await getPostById(postId);
-    return postSnapshot.exists;
-}
+const { getAllPosts } = require("./post.repository");
 
 const getPosts = async (page, limit) => {
-    const postsSnapshot = await getAllPosts(page, limit);
-    const postsData = postsSnapshot.docs.map((doc) => {
-        const data = doc.data();
-  
-        // Convert `created_at` to string if it exists
-        if (data.created_at) {
-          data.created_at = data.created_at.toDate().toISOString(); // Convert Firestore Timestamp to ISO string
-        }
-  
-        return { id: doc.id, ...data }; // Include the document ID
+  const postsSnapshot = await getAllPosts(page, limit);
+  const postsData = [];
+
+  for (const postSnapshot of postsSnapshot.docs) {
+    const postData = postSnapshot.data();
+
+    // Resolve the user reference to actual user data
+    if (postData.user) {
+      const userSnapshot = await postData.user.get();
+      if (userSnapshot.exists) {
+        _temp = userSnapshot.data();
+        console.log(_temp);
+        postData.user = {
+          id: userSnapshot.id,
+          fullName: _temp.fullName,
+        };
+      } else {
+        postData.user = null;
+      }
+    }
+
+    // Resolve the likes reference to acutal likes data
+    if (postData.likes && Array.isArray(postData.likes)) {
+      const likesSnapshots = await Promise.all(
+        postData.likes.map((ref) => ref.get())
+      );
+      postData.likes = likesSnapshots
+        .filter((snapshot) => snapshot.exists)
+        .map((snapshot) => snapshot.id);
+    }
+
+    // Resolve the dislikes reference to acutal dislikes data
+    if (postData.dislikes && Array.isArray(postData.dislikes)) {
+      const dislikesSnapshot = await Promise.all(
+        postData.dislikes.map((ref) => ref.get())
+      );
+      postData.dislikes = dislikesSnapshot
+        .filter((snapshot) => snapshot.exists)
+        .map((snapshot) => snapshot.id);
+    }
+
+    // Convert created_at to ISO String
+    if (postData.created_at) {
+      postData.created_at = postData.created_at.toDate().toISOString();
+    }
+
+    postsData.push({
+      id: postSnapshot.id,
+      ...postData,
     });
-  
-      return postsData;
+  }
+
+  return postsData;
 };
 
-const getPost = async (postId) => {
-    const postSnapshot = await getPostById(postId);
-    const postData = postSnapshot.data();
-    if (postData.created_at) {
-        postData.created_at = postData.created_at.toDate().toISOString();
-    }
-    return postData;
-}
+// const isPostIdExists = async (postId) => {
+//     const postSnapshot = await getPostById(postId);
+//     return postSnapshot.exists;
+// }
 
-const getComments = async (postId, page, limit) => {
-    const commentsSnapshot = await getPostComments(postId, page, limit);
-    const commentsData = commentsSnapshot.docs.map((doc) => {
-        const data = doc.data();
-        if (data.created_at) {
-            data.created_at = data.created_at.toDate().toISOString();
-        }
-        return { id: doc.id, ...data };
-    });
-    return commentsData;
-}
+// const getPost = async (postId) => {
+//     const postSnapshot = await getPostById(postId);
+//     const postData = postSnapshot.data();
+//     if (postData.created_at) {
+//         postData.created_at = postData.created_at.toDate().toISOString();
+//     }
+//     return postData;
+// }
 
-const getLikes = async (postId) => {
-    const likesSnapshot = await getPostLikes(postId);
-    return likesSnapshot.data();
-}
+// const getComments = async (postId, page, limit) => {
+//     const commentsSnapshot = await getPostComments(postId, page, limit);
+//     const commentsData = commentsSnapshot.docs.map((doc) => {
+//         const data = doc.data();
+//         if (data.created_at) {
+//             data.created_at = data.created_at.toDate().toISOString();
+//         }
+//         return { id: doc.id, ...data };
+//     });
+//     return commentsData;
+// }
 
-const getDislikes = async (postId) => {
-    const dislikesSnapshot = await getPostDislikes(postId);
-    return dislikesSnapshot.data();
-}
+// const getLikes = async (postId) => {
+//     const likesSnapshot = await getPostLikes(postId);
+//     return likesSnapshot.data();
+// }
 
-const createPost = async (post) => {
-    // add like and dislike 0 to post
-    post = {...post, likes: 0, dislikes: 0};
-    await createNewPost(post);
-}
+// const getDislikes = async (postId) => {
+//     const dislikesSnapshot = await getPostDislikes(postId);
+//     return dislikesSnapshot.data();
+// }
 
-const createComment = async (postId, comment) => {
-    comment = {...comment, likes: 0, dislikes: 0};
-    await createNewComment(postId, comment);
-}
+// const createPost = async (post) => {
+//     // add like and dislike 0 to post
+//     post = {...post, likes: 0, dislikes: 0};
+//     await createNewPost(post);
+// }
 
-const updatePost = async (postId, post) => {
-    await updatePostById(postId, post);
-}
+// const createComment = async (postId, comment) => {
+//     comment = {...comment, likes: 0, dislikes: 0};
+//     await createNewComment(postId, comment);
+// }
 
-const deletePost = async (postId) => {
-    await deletePostById(postId);
-}
+// const updatePost = async (postId, post) => {
+//     await updatePostById(postId, post);
+// }
+
+// const deletePost = async (postId) => {
+//     await deletePostById(postId);
+// }
 
 // const LikeToPost = async (postId) => {
 //     const postSnapshot = await getPostById(postId);
@@ -137,18 +167,6 @@ const deletePost = async (postId) => {
 //     await updateCommentById(postId, commentId, comment);
 // }
 
-
-
 module.exports = {
-    getPosts,
-    getPost,
-    getComments,
-    getLikes,
-    getDislikes,
-    createPost,
-    createComment,
-    updatePost,
-    deletePost,
-    isPostIdExists
-    
+  getPosts,
 };
